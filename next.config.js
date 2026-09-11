8d9c93fe8f26a8b5e7afe02ecd2c84e2b23d6fb5
Kate <dhwanksh2000@gmail.com>
2026-09-12 00:47:26 +0530

    feat: allow cached Wikimedia city thumbnails

--- next.config.js
+++ next.config.js
@@ -30,6 +30,7 @@ const nextConfig = {
 		formats: ["image/avif", "image/webp"],
 		remotePatterns: [
 			{ protocol: "https", hostname: "images.unsplash.com" },
+			{ protocol: "https", hostname: "upload.wikimedia.org" },
 		],
 	},
 
