ebbdadd9b1b02fa4609272efc663d43c122b4cb4
Kate <dhwanksh2000@gmail.com>
2026-09-18 03:07:30 +0530

    FIX | BUG-GEN3-RELEASE-005 allow EngineImage quality presets

--- src/engine/plugins/enginePlugin.js
+++ src/engine/plugins/enginePlugin.js
@@ -10,10 +10,28 @@ const {
 	inspectPluginArtifactCache,
 } = require("./artifactCache");
 
+const ENGINE_IMAGE_QUALITIES = Object.freeze([65, 75, 78, 90]);
+
+function withEngineImageConfig(nextConfig = {}) {
+	const configuredQualities = Array.isArray(nextConfig.images?.qualities)
+		? nextConfig.images.qualities.filter((quality) => Number.isInteger(quality) && quality >= 1 && quality <= 100)
+		: [];
+	const qualities = [...new Set([...configuredQualities, ...ENGINE_IMAGE_QUALITIES])]
+		.sort((left, right) => left - right);
+	return {
+		...nextConfig,
+		images: {
+			...(nextConfig.images || {}),
+			qualities,
+		},
+	};
+}
+
 function withEngine(nextConfig = {}, pluginOptions = {}) {
+	const imageConfig = withEngineImageConfig(nextConfig);
 	const apiOptions = pluginOptions.api || pluginOptions;
 	const shaderOptions = pluginOptions.shader || pluginOptions;
-	const apiConfig = withEngineAPI(nextConfig, apiOptions);
+	const apiConfig = withEngineAPI(imageConfig, apiOptions);
 	const nencConfig = pluginOptions.nenc ? withEngineNENC(apiConfig, pluginOptions.nenc) : apiConfig;
 	const debugConfig = withEngineDebug(nencConfig, pluginOptions.debug || {});
 	const seoConfig = withEngineSEO(debugConfig, pluginOptions.seo);
@@ -30,5 +48,6 @@ module.exports.withEngineDebug = withEngineDebug;
 module.exports.withEngineSEO = withEngineSEO;
 module.exports.withEngineShader = withEngineShader;
 module.exports.withEngineNENC = withEngineNENC;
+module.exports.withEngineImageConfig = withEngineImageConfig;
 module.exports.clearPluginArtifactCache = clearPluginArtifactCache;
 module.exports.inspectPluginArtifactCache = inspectPluginArtifactCache;
